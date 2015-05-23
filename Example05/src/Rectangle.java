e70a47cd614b873534d31b6367b9984971a4ae72
Admin <Admin@D20911>
2015-05-23 14:34:20 +0700

    Abc

---
+++ Example05/src/Rectangle.java
@@ -0,0 +1,12 @@
+
+public class Rectangle extends Polygon{
+	
+	
+	public Rectangle(int w,int h) {
+		super(w);
+		// TODO Auto-generated constructor stub
+	}
+	public int area (){
+		return (width*height);
+	}
+}
