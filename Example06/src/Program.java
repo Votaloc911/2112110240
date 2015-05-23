73a84b963be130b3773246df3cd2dd64bed28e4d
Admin <Admin@D20911>
2015-05-23 14:35:00 +0700

    Abc

---
+++ Example06/src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		Polygon po=new Polygon(8, 9);
+		po.setValues(4, 5);
+		return;
+
+	}
+
+}
---
+++ Example06/src/Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle extends Polygon {
+
+	public Rectangle(int w, int h){
+		super (w, h);
+		height=h;
+	}
+public int area(){
+	return(width*height);
+}
+}
