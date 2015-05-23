f1af934546ef0bff1422a6a8dde3ee700ad64269
Admin <Admin@D20911>
2015-05-23 14:27:01 +0700

    ac

---
+++ Example01/src/Crectangle.java
@@ -0,0 +1,13 @@
+
+public class Crectangle {
+int x,y;
+public void setValues (int a,int b)
+{
+	x=a;
+	y=b;
+	
+}
+public int area(){
+	return (x*y);
+}
+}
---
+++ Example01/src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		Crectangle rect =new Crectangle();
+		Crectangle rectb =new Crectangle();
+		rect.setValues(3, 4);
+		rect.setValues(5, 6);
+		System.out.println("rect a:"+rect.area());
+		System.out.println("rect b:"+rectb.area());
+	}
+
+}
